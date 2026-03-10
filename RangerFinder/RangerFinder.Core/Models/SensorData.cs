360a9e99424bc80e911f50959b25ab3776d27e74
Komseb <57012735+Komseb@users.noreply.github.com>
2026-03-10 08:59:34 +0100

    Add RangerFinder.Core project and Bluetooth service mock
    Added RangerFinder.Core project to solution and referenced it in main app. Introduced SensorData record, IBluetoothService interface, and MockBluetoothService for simulating sensor data. Updated solution and project files for integration.

---
+++ RangerFinder/RangerFinder.Core/Models/SensorData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RangerFinder.Core.Models
+{
+    public record SensorData(
+        double Ultrasonic1,
+        double Ultrasonic2,
+        double Lidar1,
+        double Lidar2,
+        double Obstacle
+        );
+}
---
+++ RangerFinder/RangerFinder.Core/Services/IBluetoothService.cs
@@ -0,0 +1,15 @@
+﻿using RangerFinder.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RangerFinder.Core.Services
+{
+    internal interface IBluetoothService
+    {
+        event EventHandler<SensorData> SensorDataReceived;
+
+        Task ConnectAsync();
+        Task DisconnectAsync();
+    }
+}
