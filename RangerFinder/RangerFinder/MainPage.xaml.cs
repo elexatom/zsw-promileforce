d5ef28af35dd3616fc1735838ef236e82683d707
TroubaKuba <jakubsoma04@gmail.com>
2026-03-23 17:21:22 +0100

    feat: implement SensorChartDrawable for real-time data plotting

--- RangerFinder/RangerFinder/MainPage.xaml.cs
+++ RangerFinder/RangerFinder/MainPage.xaml.cs
@@ -4,10 +4,27 @@ namespace RangerFinder
 {
     public partial class MainPage : ContentPage
     {
+        private SensorChartDrawable _chartDrawable;
+        private MainViewModel _viewModel;
+
         public MainPage(MainViewModel viewModel)
         {
             InitializeComponent();
-            BindingContext = viewModel;
+            
+            _viewModel = viewModel;
+            BindingContext = _viewModel;
+
+            _chartDrawable = new SensorChartDrawable();
+            _chartDrawable.History = _viewModel.SensorHistory;
+            ChartGraphicsView.Drawable = _chartDrawable;
+
+            _viewModel.PropertyChanged += (s, e) => 
+            {
+                if (e.PropertyName == nameof(MainViewModel.CurrentData))
+                {
+                    ChartGraphicsView.Invalidate();
+                }
+            };
         }
     }
 }
